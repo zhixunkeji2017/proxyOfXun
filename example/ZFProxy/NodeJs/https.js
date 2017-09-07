b278639f9e229879d8dda0a9d96265affc1bc780
zhixunkeji2017 <31059234+zhixunkeji2017@users.noreply.github.com>
2017-09-07 01:28:41 -0500

    Create https.js

---
+++ example/ZFProxy/NodeJs/https.js
@@ -0,0 +1,37 @@
+
+const crypto = require('crypto');
+var url = require('url');
+var https = require('https');
+var HttpsProxyAgent = require('https-proxy-agent'); //第三方包,请安装
+
+let timestamp = parseInt(new Date().getTime()/1000);
+let orderno = 'ZF2017974824isWRfK';
+let secret = 'cb65091847ad42f5b8a98f832338e94c';
+
+let plantext = 'orderno='+orderno+',secret='+secret+',timestamp='+timestamp;
+let md5 = crypto.createHash('md5');
+md5.update(plantext);
+let sign = md5.digest('hex');
+sign = sign.toUpperCase();
+
+// HTTP/HTTPS proxy to connect to
+var proxy = process.env.http_proxy || 'http://10.60.20.115:7789';
+console.log('using proxy server %j', proxy);
+
+// HTTPS endpoint for the proxy to connect to
+var endpoint = process.argv[2] || 'https://www.baidu.com';
+console.log('attempting to GET %j', endpoint);
+var options = url.parse(endpoint);
+options.headers = {
+  'Proxy-Authorization':'sign='+sign+'&orderno='+orderno+"&timestamp="+timestamp
+}
+options.rejectUnauthorized = false
+
+// create an instance of the `HttpsProxyAgent` class with the proxy server information
+var agent = new HttpsProxyAgent(proxy);
+options.agent = agent;
+
+https.get(options, function (res) {
+  console.log('"response" event!', res.headers);
+  res.pipe(process.stdout);
+});
