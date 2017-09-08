62110257c156a96e680397388e596c4a2b0178b8
zhixunkeji2017 <31059234+zhixunkeji2017@users.noreply.github.com>
2017-09-07 22:35:23 -0500

    Update main.py

--- example/ZFProxy/Python/main.py
+++ example/ZFProxy/Python/main.py
@@ -4,26 +4,46 @@ import hashlib
 import requests
 import grequests
 
+
 _version = sys.version_info
- is_python3 = (_version[0] == 3)
- 
- orderno = "ZF2017941249NePjWY"
- secret = "6040074d0cde4b3daba76213dd665834"
-  
- timestamp = str(int(time.time()))                # 计算时间戳
- string = ""
- string = "orderno=" + orderno + "," + "secret=" + secret + "," + "timestamp=" + timestamp
-  
- if is_python3:
-      string = string.encode()
-     
- md5_string = hashlib.md5(string).hexdigest()                 # 计算sign
- sign = md5_string.upper()                              # 转换成大写
- print(sign)
- auth = "sign=" + sign + "&" + "orderno=" + orderno + "&" + "timestamp=" + timestamp
-  
- print(auth)
- proxy = {"http": "http://10.60.10.71:7789", "https": "https://10.60.10.71:7789"}
- headers = {"Proxy-Authorization": auth}
- r = requests.get("https://www.baidu.com", headers=headers, proxies=proxy, verify=False)
- print(r.status_code)
+
+is_python3 = (_version[0] == 3)
+
+orderno = "ZF2017985116rTEkIY"
+secret = "bbaee8bad1d44cee9621474cf8c0de96"
+
+ip = "10.60.20.115"
+port = "7789"
+
+ip_port = ip + ":" + port
+
+timestamp = str(int(time.time()))                # 计算时间戳
+string = ""
+string = "orderno=" + orderno + "," + "secret=" + secret + "," + "timestamp=" + timestamp
+
+if is_python3:                          
+    string = string.encode()
+
+md5_string = hashlib.md5(string).hexdigest()                 # 计算sign
+sign = md5_string.upper()                              # 转换成大写
+print(sign)
+auth = "sign=" + sign + "&" + "orderno=" + orderno + "&" + "timestamp=" + timestamp
+
+print(auth)
+proxy = {"http": "http://" + ip_port, "https": "https://" + ip_port}
+headers = {"Proxy-Authorization": auth}
+r = requests.get("https://www.baidu.com", headers=headers, proxies=proxy, verify=False)
+print(r.status_code)
+print(r.content)
+print(r.text)
+
+while True:
+    urls = ["http://nba.hupu.com"] * 10
+    # urls = ["https://www.baidu.com"] * 10
+    print(urls)
+    start = time.time()
+    r = (grequests.get(u, proxies=proxy, headers=headers, timeout=30, verify=False, allow_redirects=False) for u in urls)
+    r_list = grequests.map(r)
+    stop = time.time()
+    print(r_list)
+    print(stop - start)
