4afa4c3527dacdf27103f759fd41b253450fa03d
zhixunkeji2017 <31059234+zhixunkeji2017@users.noreply.github.com>
2019-11-21 14:25:55 +0800

    Update GetDynamicIP.java
    更新示例

--- example/DDproxy/JAVA/GetDynamicIP.java
+++ example/DDproxy/JAVA/GetDynamicIP.java
@@ -1,78 +1,78 @@
-                       
-    import java.io.BufferedReader;
-    import java.io.IOException;
-    import java.io.InputStreamReader;
-    import java.io.UnsupportedEncodingException;
-    import java.security.NoSuchAlgorithmException;
-    import java.util.concurrent.CountDownLatch;
-    import java.util.concurrent.Future;
+package com.test.http.dd;
 
-    import org.apache.http.HttpResponse;
-    import org.apache.http.client.config.RequestConfig;
-    import org.apache.http.client.methods.HttpGet;
-    import org.apache.http.concurrent.FutureCallback;
-    import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-    import org.apache.http.impl.nio.client.HttpAsyncClients;
-    /**
-     * 拨号示例
-     * @author Administrator
-     *
-     */
-    public class GetDynamicIP {
-      //url规则为http://api.xdaili.cn/xdaili-api/privateProxy/getDynamicIP/{orderno}/{proxyId}
-    	private static String url = "http://api.xdaili.cn/xdaili-api/privateProxy/getDynamicIP/FREE0000000000007/56909da9b79811e6802371d9ec16a600";
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.concurrent.FutureCallback;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
 
-    	public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-    		HttpGet get = new HttpGet(url);
-    		RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(150000).setConnectTimeout(150000).build();
-    		CloseableHttpAsyncClient httpclient = HttpAsyncClients.custom().setDefaultRequestConfig(requestConfig).build();
-    		try {
-    			httpclient.start();
-    			final CountDownLatch latch = new CountDownLatch(1);
-    			final StringBuilder result = new StringBuilder();
-    			Future<HttpResponse> future = httpclient.execute(get, new FutureCallback<HttpResponse>() {
-    				@Override
-    				public void completed(final HttpResponse response) {
-    					BufferedReader reader = null;
-    					try {
-    						reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Future;
+/**
+ * 拨号示例
+ * @author Administrator
+ *
+ */
+public class GetDynamicIP {
+    //url规则为http://api.xdaili.cn/xdaili-api/privateProxy/getDynamicIP/{orderno}/{proxyId}
+    private static String url = "http://api.xdaili.cn/xdaili-api/privateProxy/getDynamicIP/DD201911218432z9iG9s/433a83d2f96d12e9af127cd30abda612";
 
-    						String line = null;
-    						while ((line = reader.readLine()) != null) {
-    							result.append(line + "\n");
-    						}
-    						System.out.println(result);
-    					} catch (IOException e) {
-    					} finally {
-    						try {
-    							reader.close();
-    						} catch (IOException e) {
-    						}
-    					}
-    					latch.countDown();
-    				}
+    public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        HttpGet get = new HttpGet(url);
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(150000).setConnectTimeout(150000).build();
+        CloseableHttpAsyncClient httpclient = HttpAsyncClients.custom().setDefaultRequestConfig(requestConfig).build();
+        try {
+            httpclient.start();
+            final CountDownLatch latch = new CountDownLatch(1);
+            final StringBuilder result = new StringBuilder();
+            Future<HttpResponse> future = httpclient.execute(get, new FutureCallback<HttpResponse>() {
+                @Override
+                public void completed(final HttpResponse response) {
+                    BufferedReader reader = null;
+                    try {
+                        reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 
-    				@Override
-    				public void failed(final Exception ex) {
-    					latch.countDown();
-    				}
+                        String line = null;
+                        while ((line = reader.readLine()) != null) {
+                            result.append(line + "\n");
+                        }
+                        System.out.println(result);
+                    } catch (IOException e) {
+                    } finally {
+                        try {
+                            reader.close();
+                        } catch (IOException e) {
+                        }
+                    }
+                    latch.countDown();
+                }
 
-    				@Override
-    				public void cancelled() {
-    					latch.countDown();
-    				}
+                @Override
+                public void failed(final Exception ex) {
+                    latch.countDown();
+                }
 
-    			});
-    			latch.await();
-    		} catch (Exception e) {
-    		} finally {
-    			try {
-    				httpclient.close();
-    			} catch (IOException e) {
+                @Override
+                public void cancelled() {
+                    latch.countDown();
+                }
 
-    			}
-    		}
+            });
+            latch.await();
+        } catch (Exception e) {
+        } finally {
+            try {
+                httpclient.close();
+            } catch (IOException e) {
 
-    	}
+            }
+        }
+
     }
-                 
+}
